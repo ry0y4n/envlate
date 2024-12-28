415c3555554b2417fc706c27e82b4dc17c468fff
ry0y4n <rusk.ryoy4n@gmail.com>
2024-12-28 16:35:33 +0900

    scan each line

--- src/main.go
+++ src/main.go
@@ -1,20 +1,37 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	// Load `.env file
-	data, err := os.ReadFile(".env")
+	file, err := os.Open(".env")
 	if err != nil {
-		fmt.Println("Error reading .env file: ", err)
+		fmt.Println("Can't open .env file ", err)
 		return
 	}
+	defer file.Close()
 
-	// Print the content of the file
-	fmt.Println("=== Content of .env file ===")
-	fmt.Println(string(data))
-	fmt.Println("=============================")
+	// Parse `.env` file
+	fmt.Println("=====Parse `.env` file=====")
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		trimmedLine := strings.TrimSpace(line)
+		if trimmedLine == "" || strings.HasPrefix(trimmedLine, "#") {
+			continue
+		}
+
+		fmt.Println("Each line: ", trimmedLine)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error loading .env file: ", err)
+	}
+	fmt.Println("=========================")
 }
